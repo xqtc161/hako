5c5c529154b1af4f58b0c1b339e199da7f334f70
xqtc <tila@heroin.trade>
2026-08-31 16:48:49 +0200

    chore(build): emit all commands as separate binaries in debug mode

--- build.zig
+++ build.zig
@@ -4,21 +4,56 @@ pub fn build(b: *std.Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
-    const exe = b.addExecutable(.{
-        .name = "hako",
-        .root_module = b.createModule(.{
-            .root_source_file = b.path("src/main.zig"),
-            .target = target,
-            .optimize = optimize,
-            .imports = &.{},
-        }),
+    const mod = b.createModule(.{
+        .root_source_file = b.path("src/main.zig"),
+        .target = target,
+        .optimize = optimize,
+        .imports = &.{},
     });
 
-    b.installArtifact(exe);
+    const hako = b.addExecutable(.{
+        .name = "hako",
+        .root_module = mod,
+    });
+    b.installArtifact(hako);
+
+    if (optimize == .Debug) {
+        const watchfd = b.addExecutable(.{
+            .name = "watchfd",
+            .root_module = mod,
+        });
+
+        const dumpfd = b.addExecutable(.{
+            .name = "dumpfd",
+            .root_module = mod,
+        });
+        const listen = b.addExecutable(.{
+            .name = "listen",
+            .root_module = mod,
+        });
+        const pwait = b.addExecutable(.{
+            .name = "pwait",
+            .root_module = mod,
+        });
+        const when = b.addExecutable(.{
+            .name = "when",
+            .root_module = mod,
+        });
+        const ports = b.addExecutable(.{
+            .name = "ports",
+            .root_module = mod,
+        });
+        b.installArtifact(watchfd);
+        b.installArtifact(dumpfd);
+        b.installArtifact(listen);
+        b.installArtifact(pwait);
+        b.installArtifact(when);
+        b.installArtifact(ports);
+    }
 
     const run_step = b.step("run", "Run the app");
 
-    const run_cmd = b.addRunArtifact(exe);
+    const run_cmd = b.addRunArtifact(hako);
     run_step.dependOn(&run_cmd.step);
 
     run_cmd.step.dependOn(b.getInstallStep());
@@ -28,7 +63,7 @@ pub fn build(b: *std.Build) void {
     }
 
     const exe_tests = b.addTest(.{
-        .root_module = exe.root_module,
+        .root_module = hako.root_module,
     });
 
     const run_exe_tests = b.addRunArtifact(exe_tests);
